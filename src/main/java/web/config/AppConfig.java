51f6dcc1f8568c49ffe2df0acb90f299e0d62fd7
gnom4832 <gnom4832@gmail.com>
2023-06-12 18:49:07 +0300

    task почти решен

--- src/main/java/web/config/AppConfig.java
+++ src/main/java/web/config/AppConfig.java
@@ -1,6 +1,9 @@
 package web.config;
 
 
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import web.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -13,11 +16,57 @@ import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
 
 @Configuration
+@PropertySource("classpath:db.properties")
+@EnableTransactionManagement // означает, что классы, помеченные @Transactional, должны быть обернуты аспектом транзакций.
+@ComponentScan(value = "web")
+public class AppConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("db.driver"));
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.username"));
+        dataSource.setPassword(env.getProperty("db.password"));
+
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(getDataSource());
+        entityManagerFactoryBean.setPackagesToScan("web.model"); // replace com.example.demo with your package name
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Properties jpaProperties = new Properties();
+        jpaProperties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        jpaProperties.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        jpaProperties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+
+        entityManagerFactoryBean.setJpaProperties(jpaProperties);
+
+        return entityManagerFactoryBean;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+}
+
+/*@Configuration
 @PropertySource("classpath:db.properties")
 @EnableTransactionManagement // означает, что классы, помеченные @Transactional, должны быть обернуты аспектом транзакций.
 @ComponentScan(value = "web")
@@ -58,5 +107,4 @@ public class AppConfig {
         transactionManager.setSessionFactory(getSessionFactory().getObject());
         return transactionManager;
     }
-}
-
+}*/
--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -5,10 +5,58 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import web.model.User;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import java.util.ArrayList;
 import java.util.List;
 @Repository //создаст бин класс и внедрит в контроллер
+public class UserDaoImpl implements UserDao{
+    // здесь логика для работы с БД
+
+    @PersistenceContext()
+    private EntityManager entityManager;
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> index() {
+        TypedQuery<User> query = entityManager.createQuery("from User", User.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public User show(int id) { //находим человека с id
+
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public void save(User user) { // добавляем новый объект класса User в БД
+        entityManager.persist(user);
+    }
+
+    // добавляем метод обновления usera В БД
+    @Override
+    public void update(int id, User updatedUser) {
+        User userToBeUbdated=show(id);// находим человека по id c помощью show и помещаем его в userToBeUbdated
+        userToBeUbdated.setName(updatedUser.getName()); // обновляем у него имя
+        userToBeUbdated.setEmail(updatedUser.getEmail()); // обновляем у него почту
+        entityManager.merge(userToBeUbdated);
+    }
+
+    // добавляем метод удаления usera В БД
+    @Override
+    public void delete(int id) {
+        entityManager.remove(show(id)); // удаляет  по предикату
+        // проходимся  по каждому человеку из списка, и если id авен пришедшемку id то user удаляется
+
+    }
+
+}
+
+
+
+
+/*@Repository //создаст бин класс и внедрит в контроллер
 public class UserDaoImpl implements UserDao{
     // здесь логика для работы с БД
 
@@ -47,7 +95,7 @@ public class UserDaoImpl implements UserDao{
 
     }
 
-}
+}*/
 
 
 
